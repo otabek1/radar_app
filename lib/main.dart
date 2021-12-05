2e26ac2d8bcd5ba8c4b1702d5b9618abf4ee4ff5
grand007hunter@gmail.com <>
2021-12-05 05:09:52 -0500

    improve ui

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _AppState extends State<App> {
         if (snapshot.connectionState == ConnectionState.done) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'FlutterFire Demo',
+            title: 'BeSafe',
             home: FireMap(),
           );
         }
@@ -59,10 +59,10 @@ class _AppState extends State<App> {
         return MaterialApp(
           home: Scaffold(
             appBar: AppBar(
-              title: const Text('FlutterFire Loading'),
+              title: const Text('BeSafe Loading'),
             ),
             body: const Center(
-              child: Text('Loading...'),
+              child: Text('Yuklanmoqda...'),
             ),
           ),
         );
